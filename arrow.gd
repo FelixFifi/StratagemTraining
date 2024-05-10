7192a939428e59b143beab36c08de8cd03446a85
FelixFifi <FelixFifi@users.noreply.github.com>
2024-05-11 00:16:04 +0200

    Now contains infinite sequences

--- arrow.gd
+++ arrow.gd
@@ -8,13 +8,24 @@ enum EArrowDirection {
 	LEFT = 180,
 }
 
-var arrow_direction : EArrowDirection = EArrowDirection.UP
+@export var arrow_direction : EArrowDirection = EArrowDirection.UP
 
 
 func set_direction(direction: EArrowDirection):
-	var arrow_up = $ArrowUp
+	var arrow_up: TextureRect = $ArrowUp_Texture
 	arrow_up.rotation_degrees = direction
 	arrow_direction = direction
+	if direction in [EArrowDirection.UP, EArrowDirection.DOWN]:
+		custom_minimum_size = Vector2(arrow_up.texture.get_height(), arrow_up.texture.get_width())
+	else:
+		custom_minimum_size = Vector2(arrow_up.texture.get_width(), arrow_up.texture.get_height())
+	pivot_offset = custom_minimum_size / 2.0
+	size = custom_minimum_size
 
 func get_direction():
 	return arrow_direction
+
+func mark_done():
+	var texture : Texture = load("res://textures/texture_done_right.tres")
+	var arrow_up = $ArrowUp_Texture
+	arrow_up.texture = texture
--- controller.gd
+++ controller.gd
@@ -9,32 +9,45 @@ const DIRECTION_ACTION_MAPPING = {
 
 
 var arrows : Array[Arrow]= []
+var arrows_done : Array[Arrow]= []
 @onready var code = %Code
 var arrow_scene = load("res://arrow.tscn")
 
 
 
 func _ready():
-	for i in range(5):
+	generate_arrows(5)
+
+func generate_arrows(n: int):
+	for i in range(n):
 		var arrow: Arrow = arrow_scene.instantiate()
-		arrow.set_direction(Arrow.EArrowDirection.DOWN)
+		var direction : Arrow.EArrowDirection = DIRECTION_ACTION_MAPPING.keys().pick_random()
+		arrow.set_direction(direction)
 		code.add_child(arrow)
 		arrows.append(arrow)
 
 
+func cleanup_arrows():
+	for arrow in arrows_done:
+		arrow.queue_free()
+	arrows_done.clear()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if len(arrows) == 0:
+		cleanup_arrows()
+		generate_arrows(10)
 
 
 func _unhandled_key_input(event):
 	if len(arrows) > 0:
 		var arrow = arrows[0]
 
-		var required_input =DIRECTION_ACTION_MAPPING[arrow.get_direction()]
+		var required_input = DIRECTION_ACTION_MAPPING[arrow.get_direction()]
 
 		if event.is_action_pressed(required_input):
-			arrow.queue_free()
-			arrows.pop_front()
+			arrow.mark_done()
+			arrows_done.append(arrows.pop_front())
 
 
